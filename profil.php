77deb48b216765fdb8a2e0af2fe902dddfb98b1c
MhadidIman <hadidmuhamad417@gmail.com>
2024-06-05 13:30:40 +0800

    Initial commit

---
+++ profil.php
@@ -0,0 +1,54 @@
+<!-- <?php include "header.php" ?> -->
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/hadid.jpg" alt="Adul Mancing" width="170" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK / NIDN :</strong></td>
+                        <td align="left" width="50%">22 100100 68 / 2210010068</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Informatika :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">M. Hadid Iman Firdaus</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">BalaiRiam, 21 Desember 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">087798645424</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">whullhull@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/muhamadhadid.blezhh" target="_blank" style="text-decoration:none;">
+                                <img src="img/facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/mhmdhdid___" target="_blank" style="text-decoration:none;">
+                                <img src="img/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/dedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/dedyrosyadi" target="_blank">
+                                <img src="img/linkedin.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
